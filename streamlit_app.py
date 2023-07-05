1b159d21ec35744cb399fa3e9d5839a35e1d0179
aishwarya2819 <99477890+aishwarya2819@users.noreply.github.com>
2023-07-05 23:53:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,26 +41,27 @@ streamlit.dataframe(fruits_to_show)
 # write your own comment - what does this do?
 # streamlit.dataframe(fruityvice_normalized)
 
-streamlit.header("Fruityvice Fruit Advice!")
-# try:
-#   fruit_choice= streamlit.text_input('What fruit would you like information about?')
-#   # my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values(add_fruit)")
-#   if not fruit_choice:
-#     streamlit.error("please select fruit to get information")
-#   else:
-#     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#     streamlit.dataframe(fruityvice_normalized)
-# except URLError as e:
-#   streamlit.error()
+
+
 
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
   fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
-
-back_from_function=get_fruityvice_data(fruit_choice)
-streamlit.dataframe(back_from_function)
+  
+streamlit.header("Fruityvice Fruit Advice!")  
+try:
+  fruit_choice= streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("please select fruit to get information")
+  else:
+    # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # streamlit.dataframe(fruityvice_normalized)
+    back_from_function=get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
