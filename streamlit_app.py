9f9cec3e75bdbb224f3791116c36a898ebdb86ec
aishwarya2819 <99477890+aishwarya2819@users.noreply.github.com>
2023-07-06 00:21:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,34 +44,34 @@ streamlit.dataframe(fruits_to_show)
 
 
 
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
-  fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
+# def get_fruityvice_data(this_fruit_choice):
+#   fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+#   fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+#   return fruityvice_normalized
   
-streamlit.header("Fruityvice Fruit Advice!")  
-try:
-  fruit_choice= streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("please select fruit to get information")
-  else:
-    # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # streamlit.dataframe(fruityvice_normalized)
-    back_from_function=get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
-except URLError as e:
-  streamlit.error()
+# streamlit.header("Fruityvice Fruit Advice!")  
+# try:
+#   fruit_choice= streamlit.text_input('What fruit would you like information about?')
+#   if not fruit_choice:
+#     streamlit.error("please select fruit to get information")
+#   else:
+#     # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#     # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#     # streamlit.dataframe(fruityvice_normalized)
+#     back_from_function=get_fruityvice_data(fruit_choice)
+#     streamlit.dataframe(back_from_function)
+# except URLError as e:
+#   streamlit.error()
 
-def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from fruit_load_list")
-    return my_cur.fetchall()
+# def get_fruit_load_list():
+#   with my_cnx.cursor() as my_cur:
+#     my_cur.execute("select * from fruit_load_list")
+#     return my_cur.fetchall()
 
-if streamlit.button('Get Fruit Load List'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows=get_fruit_load_list()
-  streamlit.dataframe(my_data_rows)
+# if streamlit.button('Get Fruit Load List'):
+#   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#   my_data_rows=get_fruit_load_list()
+#   streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
