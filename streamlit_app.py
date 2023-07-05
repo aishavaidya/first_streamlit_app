f2009349bd27f6c0054b40be9f1659c694893689
aishwarya2819 <99477890+aishwarya2819@users.noreply.github.com>
2023-07-05 23:44:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,17 +42,17 @@ streamlit.dataframe(fruits_to_show)
 # streamlit.dataframe(fruityvice_normalized)
 
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice= streamlit.text_input('What fruit would you like information about?')
-  # my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values(add_fruit)")
-  if not fruit_choice:
-    streamlit.error("please select fruit to get information")
-  else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-  streamlit.error()
+# try:
+#   fruit_choice= streamlit.text_input('What fruit would you like information about?')
+#   # my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values(add_fruit)")
+#   if not fruit_choice:
+#     streamlit.error("please select fruit to get information")
+#   else:
+#     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#     streamlit.dataframe(fruityvice_normalized)
+# except URLError as e:
+#   streamlit.error()
 
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
